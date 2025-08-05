337593fa07a208d5c024933acaa86250f3d2037e
Andrew Criddle <acriddl2@byu.edu>
2025-08-05 16:23:52 -0600

    Implement Connection.

--- server/src/main/java/server/websocket/Connection.java
+++ server/src/main/java/server/websocket/Connection.java
@@ -1,4 +1,19 @@
 package server.websocket;
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
 
 public class Connection {
+    public String username;
+    public String authToken;
+    public Session session;
+    public Connection(String username, String authToken, Session session) {
+        this.username = username;
+        this.authToken = authToken;
+        this.session = session;
+    }
+
+    public void send(String message) throws IOException {
+        session.getRemote().sendString(message);
+    }
 }
--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,4 +1,11 @@
 package server.websocket;
 
+import java.util.concurrent.ConcurrentHashMap;
+import org.eclipse.jetty.websocket.api.Session;
+
 public class ConnectionManager {
+    public ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+    public void add(String username, String authToken, Session session) {
+        Connection connection = new Connection(username, authToken, session);
+    }
 }
